f485d2d9cec24b8985f60c6b277f41d14f82e8f2
The Matrix <helm.chris82@gmail.com>
2020-03-19 13:12:14 -0400

    added all files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Workout-Tracker
+
+Welcome to the Workout Tracker, an app that lets you track your daily workout routine.
+
+As a fitness geek, you want to be able to view, create, and track your daily workouts. With this app you are able to log multiple exercises in a workout on a given day. You should also be able to track the name, type, weight, sets, reps, and duration of exercise.
+
+Technologies: Mongo / Mongoose / ExpressJS / NodeJS
---
+++ routes/api.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const Workout = require('../models/workout');
+
+router.post('/api/workouts', (req, res) => {
+  console.log(req.body);
+  Workout.create({
+    exercises: []
+  })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+  console.log('connected to put');
+  console.log(body);
+  Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: { exercises: body }
+    },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      console.log('after promise');
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get('/api/workout', (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put('/api/workouts/:id', function(req, res) {
+  console.log(req.body);
+  db.Workout.where('_id', req.params.id)
+    .update({ $push: { exercises: req.body } })
+    .then(function(results) {
+      res.json(results);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+module.exports = router;
